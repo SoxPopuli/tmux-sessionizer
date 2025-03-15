31846815c1f738473d931b7f452283a3f7bc3cea
Charlotte Smith <charliesmith5019@gmail.com>
2025-03-15 19:23:39 +0000

    Add benchmark

---
+++ benches/benchmark.rs
@@ -0,0 +1,42 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+
+trait SearchPathHelper {
+    fn simple(path: impl Into<String>) -> SearchPath {
+        SearchPath::Simple(path.into()).expand().unwrap()
+    }
+
+    fn complex(path: impl Into<String>, depth: Option<u8>) -> SearchPath {
+        SearchPath::Complex {
+            path: path.into(),
+            depth,
+        }
+        .expand()
+        .unwrap()
+    }
+}
+impl SearchPathHelper for SearchPath {}
+
+use tmux_sessionizer::config::{Config, SearchPath, Settings};
+
+fn find_all_dirs(c: &mut Criterion) {
+    let config = Config {
+        paths: vec![
+            SearchPath::simple("~/Code"),
+            SearchPath::simple("~/Documents/Work"),
+            SearchPath::complex("~/Documents", Some(1)),
+            SearchPath::complex("~/.config", Some(1)),
+            SearchPath::complex("~/vaults", Some(0)),
+        ],
+        settings: Settings {
+            default_depth: 8,
+            picker: None,
+        },
+    };
+
+    c.bench_function("find_dirs", |b| {
+        b.iter(|| black_box(config.find_dirs().unwrap()));
+    });
+}
+
+criterion_group!(benches, find_all_dirs);
+criterion_main!(benches);
--- src/main.rs
+++ src/main.rs
@@ -1,139 +1,14 @@
+mod config;
 mod error;
+use config::{Config, Settings};
 use error::Error;
 mod tmux;
 
 use std::{
-    env::VarError,
-    fs::File,
     io::Write,
     path::{Path, PathBuf},
     process::{Command, Stdio},
 };
-
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-#[serde(untagged)]
-pub enum SearchPath {
-    Simple(String),
-    Complex { path: String, depth: Option<u8> },
-}
-impl SearchPath {
-    pub fn depth(&self, default: u8) -> u8 {
-        match self {
-            Self::Simple(_) => default,
-            Self::Complex { depth, .. } => depth.unwrap_or(default),
-        }
-    }
-
-    pub fn path(&self) -> &str {
-        match self {
-            Self::Simple(s) => s.as_str(),
-            Self::Complex { path, .. } => path.as_str(),
-        }
-    }
-
-    pub fn expand(&self) -> Result<Self, Error> {
-        fn expand(s: &str) -> Result<String, Error> {
-            shellexpand::full(s)
-                .map_err(|e| Error::EnvError(e.to_string()))
-                .map(|s| s.to_string())
-        }
-
-        match self {
-            Self::Simple(s) => Ok(Self::Simple(expand(s)?)),
-            Self::Complex { path, depth } => Ok(Self::Complex {
-                path: expand(path)?,
-                depth: *depth,
-            }),
-        }
-    }
-}
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-struct Settings {
-    default_depth: u8,
-    picker: Option<String>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-struct Config {
-    settings: Settings,
-    paths: Vec<SearchPath>,
-}
-impl Config {
-    const CONFIG_FILE_NAME: &str = "tms";
-
-    pub fn try_open() -> Result<Self, Error> {
-        let config_file = std::env::var("TMS_CONFIG")
-            .map_err(|e| match e {
-                VarError::NotUnicode(e) => panic!("{e:?}"),
-                VarError::NotPresent => Error::MissingHome,
-            })
-            .and_then(|path| File::open(path).map_err(|e| Error::FileError(e.to_string())));
-
-        fn read_file(file: File) -> Result<Config, Error> {
-            serde_yml::from_reader(file).map_err(|e| Error::file_error(e.to_string()))
-        }
-
-        if let Ok(config) = config_file {
-            return read_file(config);
-        }
-
-        let home = std::env::var("HOME").expect("'HOME' env var not found");
-        let config_path = PathBuf::from(home).join(".config");
-
-        let possible_file_names = [
-            format!("{}.yml", Self::CONFIG_FILE_NAME),
-            format!("{}.yaml", Self::CONFIG_FILE_NAME),
-        ];
-
-        possible_file_names
-            .into_iter()
-            .find_map(|name| File::open(config_path.join(name)).ok())
-            .ok_or_else(|| Error::file_error("Missing config file '~/.config/tms.yml'"))
-            .and_then(read_file)
-    }
-
-    fn find_dir_recursive(path: &Path, depth: u8, max_depth: u8) -> impl Iterator<Item = PathBuf> {
-        path.read_dir()
-            .unwrap()
-            .flatten()
-            .filter(|d| d.file_type().map(|ft| ft.is_dir()).unwrap_or(false))
-            .flat_map(move |e| {
-                let path = e.path();
-                if depth < max_depth {
-                    let mut paths = vec![path.clone()];
-                    paths.extend(Self::find_dir_recursive(&path, depth + 1, max_depth));
-                    paths
-                } else {
-                    vec![path]
-                }
-            })
-    }
-
-    fn find_dirs(&self) -> Result<Vec<PathBuf>, Error> {
-        let paths = self
-            .paths
-            .iter()
-            .map(|path| path.expand())
-            .collect::<Result<Vec<_>, _>>()?;
-
-        let mut all_paths = vec![];
-
-        for p in paths {
-            let path = Path::new(p.path());
-            if !path.exists() {
-                continue;
-            }
-            let depth = p.depth(self.settings.default_depth);
-            let p = Self::find_dir_recursive(path, 1, depth);
-            all_paths.extend(p);
-        }
-
-        Ok(all_paths)
-    }
-}
 
 fn run_finder(Settings { picker, .. }: &Settings, paths: &[PathBuf]) -> Option<PathBuf> {
     let picker = picker.as_deref().unwrap_or("fzf-tmux -p 50%");
@@ -217,6 +92,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use crate::config::SearchPath;
+
     use super::*;
 
     #[test]
